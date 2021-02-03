b509656e26d4f3be0385445a85a6a24758755b40
hanifiamdev <hanifamrullahdev@gmail.com>
2021-02-03 23:54:08 +0700

    menambahkan repo untuk belajar java dasar

---
+++ java-dasar/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author Hanif Amrullah
+ * @since 03/02/2021 - 22:22
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+
+
