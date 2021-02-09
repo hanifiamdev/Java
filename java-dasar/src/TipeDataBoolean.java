82d48fb1e26404fdc9e9306b56edc4be35593ce6
hanifiamdev <hanifamrullahdev@gmail.com>
2021-02-10 03:05:39 +0700

    add materi tipe data boolean

---
+++ java-dasar/src/TipeDataBoolean.java
@@ -0,0 +1,18 @@
+/**
+ * @author Hanif Amrullah
+ * @since 10/02/2021 - 3:00
+ */
+
+public class TipeDataBoolean {
+	public static void main(String[] args) {
+
+		boolean benar = true;
+		boolean salah = false;
+		boolean aritmatika= 1 > 5;
+
+		System.out.println(benar); // true
+		System.out.println(salah); // false
+		System.out.println(aritmatika); // false
+		
+	}
+}
