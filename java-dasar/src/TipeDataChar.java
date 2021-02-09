956ee210179b6714f82203f2bf436b95f34276ae
hanifiamdev <hanifamrullahdev@gmail.com>
2021-02-10 02:57:19 +0700

    add materi tipe data Char

---
+++ java-dasar/src/TipeDataChar.java
@@ -0,0 +1,26 @@
+/**
+ * @author Hanif Amrullah
+ * @since 10/02/2021 - 2:36
+ */
+
+public class TipeDataChar {
+	public static void main(String[] args) {
+
+		char h = 'H';
+		char a = 'A';
+		char n = 'N';
+		char i = 'I';
+		char f = 'F';
+		System.out.println("=== Output ==="); // prinln = cetak dengan new line sedangkan print hanya cetak saja
+		System.out.print(h);
+		System.out.print(a);
+		System.out.print(n);
+		System.out.print(i);
+		System.out.print(f);
+		/*
+		=== Output ===
+		HANIF
+		* */
+
+	}
+}
