aa65c630ff652a0c262adae8e539028ebb816c64
hanifiamdev <hanifamrullahdev@gmail.com>
2021-02-10 03:46:54 +0700

    add materi varible java

---
+++ java-dasar/src/Variable.java
@@ -0,0 +1,26 @@
+/**
+ * @author Hanif Amrullah
+ * @since 10/02/2021 - 3:40
+ */
+
+public class Variable {
+	public static void main(String[] args) {
+
+		String name;
+		name = "Hanif Amrullah";
+
+		int age = 17;
+		String address = "Indonesia";
+
+		System.out.println(name); // Hanif Amrullah
+		System.out.println(age); // 17
+		System.out.println(address); // Indonesia
+
+		// Mengganti value sebuah variable
+		// name = 12345  // ini eror karena name String di inputkan value int
+		name = "Fachri Amrullah";
+		System.out.println(name); // Fachri Amrullah
+
+
+	}
+}
